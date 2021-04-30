72c70905c07734893c9a23244100c60b13be3dbd
Raghav Malawat <raghavmalawat@gmail.com>
2021-05-01 01:17:07 +0530

    Icon change

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Raghav Malawat - Portfolio`,
+        name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#81e6d9`,
